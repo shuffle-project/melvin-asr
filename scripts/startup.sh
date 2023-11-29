2c1e3f3d59068875b73fcd05076eecefae6ec43c
Florian Demel <florian.demel@meondi.com>
2023-11-29 12:50:43 +0100

    add commands to sh file

---
+++ scripts/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to the directory containing the docker-compose.yml file
+cd /home/shuffle/asr-api-prod
+
+# Start Docker Compose
+docker compose down
+docker-compose up -d
+
+# Remove all unused images
+docker images -a | grep 'asr-api-api' | awk '{print $3}' | xargs -r docker rmi    
+docker images -a | grep 'asr-api-whispercpp_runner' | awk '{print $3}' | xargs -r docker rmi
