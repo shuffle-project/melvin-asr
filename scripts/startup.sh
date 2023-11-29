ccdbca9304dadb5f2f5d0f748595f0a0d9cdc0b2
Florian Demel <florian.demel@meondi.com>
2023-11-29 13:12:18 +0100

    remove auto cleanup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,7 +6,3 @@ cd /home/shuffle/asr-api-prod
 # Start Docker Compose
 docker compose down --rmi local
 docker compose up -d
-
-# Remove all unused images with name "asr-api"
-docker rmi $(docker images | grep 'asr-api')   
-
