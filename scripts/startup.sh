8062a047e477f8b9e7cb902839ae4ee0bd08c945
Florian Demel <florian.demel@meondi.com>
2023-11-29 13:09:11 +0100

    adjust remove unused images

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,9 +4,9 @@
 cd /home/shuffle/asr-api-prod
 
 # Start Docker Compose
-docker compose down
+docker compose down --rmi local
 docker compose up -d
 
-# Remove all unused images
-docker images -a | grep 'asr-api-api' | awk '{print $3}' | xargs -r docker rmi    
-docker images -a | grep 'asr-api-whispercpp_runner' | awk '{print $3}' | xargs -r docker rmi
+# Remove all unused images with name "asr-api"
+docker rmi $(docker images | grep 'asr-api')   
+
