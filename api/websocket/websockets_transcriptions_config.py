dada1f41ffd86b0f0b564e1450924a568a2aed7b
Florian Demel <florian.demel@meondi.com>
2023-12-14 12:43:41 +0100

    Make websocket print each word alone

--- api/websocket/websockets_transcriptions_config.py
+++ api/websocket/websockets_transcriptions_config.py
@@ -6,9 +6,15 @@ class WebsocketsTranscriptionsConfig:
 
     def __init__(self):
         self.language = "auto"
+        self.translate = True
+        self.split_on_word = True
+        self.max_len = 1
 
     def get_settings(self):
         """Function to get the settings"""
         return {
             "language": self.language,
+            "translate": self.translate,
+            "split_on_word": self.split_on_word,
+            "max_len": self.max_len,
         }
