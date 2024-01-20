4f969dd61e9294b6cf968fca372b23d14486afd0
Florian Demel <florian.demel@meondi.com>
2024-01-20 22:16:27 +0100

    fail test for test coverage to low

--- src/transcription/test/test_rest_runner.py
+++ src/transcription/test/test_rest_runner.py
@@ -19,20 +19,17 @@ def test_get_oldest_status_file_in_query_fail():
 
 def test_get_oldest_status_file_in_query_success(cleanup_data):
     """Tests getting the oldest status file in query returns the transcription_id."""
-    # prepare write.json file
     data = {
         "transcription_id": "write",
         "status": TranscriptionStatus.IN_QUERY.value,
         "start_time": "2021-05-01T00:00:00Z",
     }
     DATA_HANDLER.write_status_file("write", data)
-    # check status file
     assert RUNNER.get_oldest_status_file_in_query(1, DATA_HANDLER) == "write"
 
 
 def test_get_oldest_status_file_in_query_oldest_first(cleanup_data):
     """Tests getting the oldest status file in query when 2 are available."""
-    # prepare write.json file
     data = {
         "transcription_id": "writeNew",
         "status": TranscriptionStatus.IN_QUERY.value,
@@ -45,5 +42,43 @@ def test_get_oldest_status_file_in_query_oldest_first(cleanup_data):
         "start_time": "2021-04-01T00:00:00Z",
     }
     DATA_HANDLER.write_status_file("writeOld", data)
-    # check status file
     assert RUNNER.get_oldest_status_file_in_query(1, DATA_HANDLER) == "writeOld"
+
+def test_get_oldest_status_file_in_query_wrong_model(cleanup_data):
+    """
+    Tests getting the oldest status file in query 
+    with a file that has another model specified that the restrunner is using.
+    It should not be returned.
+    """
+    data = {
+        "transcription_id": "writeNew",
+        "status": TranscriptionStatus.IN_QUERY.value,
+        "start_time": "2021-05-01T00:00:00Z",
+        "model": "small"
+    }
+    DATA_HANDLER.write_status_file("writeNew", data)
+    assert RUNNER.get_oldest_status_file_in_query(1, DATA_HANDLER) == "None"
+
+def test_get_oldest_status_file_in_query_correct_model(cleanup_data):
+    """
+    Tests getting the oldest status file in query 
+    with a file that has another model specified that the restrunner is using.
+    And a file that has the model of the restrunner specified but is newer.
+    It should still return the newer one (date is closer to now).
+    """
+    data = {
+        "transcription_id": "writeNew",
+        "status": TranscriptionStatus.IN_QUERY.value,
+        "start_time": "2021-05-01T00:00:00Z",
+        "model": "tiny"
+    }
+    DATA_HANDLER.write_status_file("writeNew", data)
+    data = {
+        "transcription_id": "writeOld",
+        "status": TranscriptionStatus.IN_QUERY.value,
+        "start_time": "2021-04-01T00:00:00Z",
+        "model": "small"
+    }
+    DATA_HANDLER.write_status_file("writeOld", data)
+    # check status file
+    assert RUNNER.get_oldest_status_file_in_query(1, DATA_HANDLER) == "writeNew"
