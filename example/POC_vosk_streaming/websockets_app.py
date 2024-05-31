762682fcb428e1e84fb0d05b9a9ee1b3e0f380c9
Florian Demel <93883574+FlrnDml@users.noreply.github.com>
2024-05-31 12:06:11 +0200

    Add POCs (#93)

--- example/POC_vosk_streaming/websockets_app.py
+++ example/POC_vosk_streaming/websockets_app.py
@@ -4,13 +4,8 @@ import json
 import time
 import websockets
 
-from src.config import CONFIG
-from src.api.websocket.websockets_settings import default_websocket_settings
-from src.helper.logger import Color, Logger
-from src.helper.model_handler import ModelHandler
-from src.transcription.transcriber import Transcriber
-
-LOGGER = Logger("WebSocketServer", True, Color.BRIGHT_YELLOW)
+from websockets_settings import default_websocket_settings
+from transcriber import Transcriber
 
 # To Calculate the seconds of audio in a chunk of 16000 Hz, 2 bytes per sample and 1 channel (as typically used in Whisper):
 # 16000 Hz * 2 bytes * 1 channel = 32000 bytes per second
@@ -31,15 +26,8 @@ class WebSocketServer:
     def __init__(self, port: int, host: str = "localhost"):
         self.port = port
         self.host = host
-        LOGGER.print_log(
-            "Websocket Transcriber Config: {}".format(CONFIG["stream_runner"][0])
-        )
-        ModelHandler().setup_model(CONFIG["stream_runner"][0]["model"])
-        self.transcriber = Transcriber(
-            model_name=CONFIG["stream_runner"][0]["model"],
-            device=CONFIG["stream_runner"][0]["device"],
-            compute_type=CONFIG["stream_runner"][0]["compute_type"],
-        )
+
+        self.transcriber = Transcriber()
 
         # Cache for the last few chunks of audio
         self.recently_added_chunk_cache = b""
@@ -64,6 +52,7 @@ class WebSocketServer:
 
     async def main(self):
         async with websockets.serve(self.echo, self.host, self.port):
+            print("Server started at port {}".format(self.port))
             await asyncio.Future()  # run forever
 
     async def echo(self, websocket, path):
@@ -81,7 +70,7 @@ class WebSocketServer:
                     self.overall_audio_bytes += message
 
                     if len(self.chunk_cache) >= self.final_treshold:
-                        LOGGER.print_log("********** NEW FINAL **********")
+                        print("********** NEW FINAL **********")
                         asyncio.ensure_future(
                             self.transcribe_all_chunk_cache(
                                 websocket,
@@ -93,7 +82,7 @@ class WebSocketServer:
                         self.chunk_cache = b""
 
                     if len(self.recently_added_chunk_cache) >= self.partial_treshold:
-                        LOGGER.print_log("********** NEW PARTIAL **********")
+                        print("********** NEW PARTIAL **********")
                         # Here we need to ensure that the transcribe_chunk_partial does not run longer than the length of the chunks.
                         asyncio.ensure_future(
                             self.transcribe_chunk_partial(
@@ -105,17 +94,17 @@ class WebSocketServer:
                         self.recently_added_chunk_cache = b""
 
                 else:
-                    LOGGER.print_log("Received message: {}".format(message))
+                    print("Received message: {}".format(message))
                     await websocket.send("wrong data type")
 
             except websockets.exceptions.ConnectionClosedOK:
-                LOGGER.print_log("Client left")
+                print("Client left")
                 break
             except websockets.exceptions.ConnectionClosedError:
-                LOGGER.print_error("Client left unexpectedly")
+                print("Client left unexpectedly")
                 break
             except Exception as e:
-                LOGGER.print_error("Error while receiving message: {}".format(e))
+                print("Error while receiving message: {}".format(e))
                 break
 
     async def transcribe_all_chunk_cache(
@@ -146,7 +135,7 @@ class WebSocketServer:
             result = json.dumps({"result": result, "text": text}, indent=2)
         await websocket.send(result)
         end_time = time.time()
-        LOGGER.print_log(
+        print(
             "Final Transcription took {:.2f} s".format(end_time - start_time)
         )
 
@@ -167,11 +156,11 @@ class WebSocketServer:
                 text += segment["text"]
             result = json.dumps({"partial": text}, indent=2)
         else:
-            LOGGER.print_error("Transcription Data is empty, no segments found")
+            print("Transcription Data is empty, no segments found")
         await websocket.send(result)
 
         end_time = time.time()
-        LOGGER.print_log(
+        print(
             "Partial transcription took {:.2f} s".format(end_time - start_time)
         )
 
@@ -181,7 +170,7 @@ class WebSocketServer:
         seconds_transcribed = len(self.overall_transcribed_bytes) / BYTES_PER_SECOND
         # Calculate the latency of the connection
         seconds_difference_received_transcribed = seconds_received - seconds_transcribed
-        LOGGER.print_log(
+        print(
             "Difference received transcribed: {}".format(
                 seconds_difference_received_transcribed
             )
@@ -192,7 +181,7 @@ class WebSocketServer:
             seconds_difference_received_transcribed
             - self.partial_treshold / BYTES_PER_SECOND
         )
-        LOGGER.print_log(
+        print(
             "Difference received, excluding Partial threshold: {}".format(
                 seconds_difference_received_transcribed_exclude_partial
             )
@@ -203,27 +192,35 @@ class WebSocketServer:
             seconds_difference_received_transcribed_exclude_partial
             > FINAL_TRANSCRIPTION_TIMEOUT - 2
         ):
-            LOGGER.print_log("Latency is too high, increasing partial threshold")
+            print("Latency is too high, increasing partial threshold")
             self.partial_treshold = self.partial_treshold * 1.5
             if self.partial_treshold > self.final_treshold:
                 self.partial_treshold = self.final_treshold
                 # this is a bad case, maximum threshold
-                LOGGER.print_error("Partial threshold is now equal to final threshold")
+                print("Partial threshold is now equal to final threshold")
 
         if (
             seconds_difference_received_transcribed_exclude_partial
             < FINAL_TRANSCRIPTION_TIMEOUT / 6
         ):
-            LOGGER.print_log("Latency is low, decreasing partial threshold")
+            print("Latency is low, decreasing partial threshold")
             self.partial_treshold = self.partial_treshold * 0.75
             if self.partial_treshold < BYTES_PER_SECOND * PARTIAL_TRANSCRIPTION_TIMEOUT:
                 self.partial_treshold = BYTES_PER_SECOND * PARTIAL_TRANSCRIPTION_TIMEOUT
-                LOGGER.print_log(
+                print(
                     "Partial threshold is now equal to the default threshold"
                 )
 
-        LOGGER.print_log(
+        print(
             "Partial threshold is now: {}".format(
                 self.partial_treshold / BYTES_PER_SECOND
             )
         )
+
+if __name__ == "__main__":
+    try:
+        server = WebSocketServer("1234")
+        asyncio.run(server.main())
+    except KeyboardInterrupt as e:
+        print("interrupted by user")
+        exit()
