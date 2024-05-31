3ebc84f40694aceab551191e9a2ba375d64b23c8
Florian Demel <florian.demel@meondi.com>
2024-05-31 12:02:27 +0200

    Add POCs

---
+++ example/POC_vosk_streaming/websockets_settings.py
@@ -0,0 +1,9 @@
+"""This handles the transcription configuration of the websockets server."""
+
+
+def default_websocket_settings():
+    """Function to get the settings for the websockets server"""
+    return {
+        "word_timestamps": True,
+        "vad_filter": True
+    }
