4c91e800a66a55f2783d64300a77ddc8072b4ce2
Florian Demel <93883574+FlrnDml@users.noreply.github.com>
2024-06-13 16:40:32 +0200

    Fix JSON-breaking Infinity entries (#96)
    Refactor segment info parser to not insert Infinity in JSON not beeing a string

--- src/transcription/transcriber.py
+++ src/transcription/transcriber.py
@@ -6,7 +6,7 @@ import wave
 from faster_whisper import WhisperModel
 from src.config import CONFIG
 from src.helper.logger import Logger, Color
-from src.transcription.segment_info_parser_streaming import parse_segments_and_info_to_dict
+from src.transcription.segment_info_parser import parse_segments_and_info_to_dict
 from src.transcription.transcription_settings import TranscriptionSettings
 
 LOGGER = Logger("Transcriber", True, Color.MAGENTA)
