a1f86bd038892b0f08858d6ea81f52c1758e01a2
Florian Demel <florian.demel@meondi.com>
2024-06-21 15:24:17 +0200

    Add POC parallel transcription

--- src/api/websocket/websockets_server.py
+++ src/api/websocket/websockets_server.py
@@ -2,12 +2,14 @@
 import asyncio
 import websockets
 from src.api.websocket.stream import Stream
+from src.helper.logger import Logger
 
 
 class WebSocketServer:
     """Class to handle a WebSocket ASR server"""
 
     async def __init__(self, host: str, port: int):
+        self.logger = Logger("WebSocketServer", True, Logger.Color.BRIGHT_CYAN)
         self.stream_counter = 0
         async with websockets.serve(self.handle_new_client, host, port):
             await asyncio.Future()  # run forever
@@ -15,5 +17,6 @@ class WebSocketServer:
     async def handle_new_client(self, websocket, path):
         """Function to handle a new client connection"""
         self.stream_counter += 1
+        self.logger.print_log(f"New Stream Client connected. Stream ID: {self.stream_counter}")
         Stream(self.stream_counter).echo(websocket=websocket, path=path)
         
