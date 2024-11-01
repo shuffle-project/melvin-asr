c09f5a52d300b75a2d2cd51ee8a526641712b94f
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2024-11-01 12:20:50 +0100

    feat(config): fix config tests

--- src/helper/test/test_config.py
+++ src/helper/test/test_config.py
@@ -9,14 +9,24 @@ from src.helper.config import read_config
 TEST_CONFIG_SUCCESS_PATH = os.getcwd() + "/src/helper/test/test_config_success.yml"
 TEST_CONFIG_RAISE_PATH = os.getcwd() + "/src/helper/test/test_config_raise.yml"
 
+
 def test_read_config_success():
     """Tests reading a config.yml successfully."""
     TEST_CONFIG = read_config(TEST_CONFIG_SUCCESS_PATH)
 
     assert TEST_CONFIG["host"] == "localhost"
-    assert TEST_CONFIG["debug"] is True
+    assert TEST_CONFIG["log_level"] == "DEBUG"
     assert TEST_CONFIG["api_keys"] == ["shuffle2024", "api_key_1", "api_key_2"]
-    assert TEST_CONFIG["rest_runner"] == [{'device': 'cpu', 'model': 'large-v3', 'compute_type': 'int8', 'device_index': 0, 'num_workers': 1, 'cpu_threads': 4}]
+    assert TEST_CONFIG["rest_runner"] == [
+        {
+            "device": "cpu",
+            "model": "large-v3",
+            "compute_type": "int8",
+            "device_index": 0,
+            "num_workers": 1,
+            "cpu_threads": 4,
+        }
+    ]
     assert TEST_CONFIG["websocket_stream"]["cpu"]["active"] is True
     assert TEST_CONFIG["websocket_stream"]["cpu"]["model"] == "tiny"
     assert TEST_CONFIG["websocket_stream"]["cpu"]["cpu_threads"] == 4
@@ -26,9 +36,14 @@ def test_read_config_success():
     assert TEST_CONFIG["websocket_stream"]["cuda"]["device_index"] == 0
     assert TEST_CONFIG["websocket_stream"]["cuda"]["worker_seats"] == 1
 
+
 def test_read_config_raise_error():
     """Tests reading a config.yml and raising an error."""
     try:
         read_config(TEST_CONFIG_RAISE_PATH)
     except ValueError as e:
-        assert str(e) == "Configuration error: 'api_keys' is not set in .env as an environment variable or as a default value"
+        assert (
+            str(e)
+            == "Configuration error: 'api_keys' is not set in .env as an environment variable or as a default value"
+        )
+
