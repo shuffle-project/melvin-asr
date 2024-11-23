a659e1a458506b39bb3a3367e801618878add866
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2024-11-23 18:23:06 +0100

    feat(benchmark): cleanup

--- tooling/helpers/file_helper.py
+++ tooling/helpers/file_helper.py
@@ -29,11 +29,5 @@ def transform_value_for_table(val, round_value=2):
     return round(val, round_value)
 
 
-def safe_divide_for_table(sum, amount, round_value=2):
-    if amount == 0:
-        return "N/A"
-    return round((sum / amount), round_value)
-
-
 def get_file_name(path, scale):
     return path.replace(f"{os.path.join(DATA_BASE_PATH, scale)}/", "")
