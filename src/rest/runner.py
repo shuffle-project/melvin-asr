2f85c76e1d743adcc35c6cb9e2a6553b6bdd0276
branchverse <maximilian.dolbaum@gmail.com>
2025-01-12 16:50:49 +0100

    refactor: move translation to runner

--- src/rest/runner.py
+++ src/rest/runner.py
@@ -3,11 +3,14 @@
 import os
 import random
 import time
-from datetime import datetime
+from datetime import datetime, timezone
+from typing import Tuple
 
 from src.helper import logger
+from src.helper.align_translation_segments import align_segments
 from src.helper.config import CONFIG
 from src.helper.data_handler import DataHandler
+from src.helper.translate import translate_text
 from src.helper.types.transcription_status import TranscriptionStatus
 from src.rest.rest_transcriber import Transcriber
 
@@ -38,24 +41,26 @@ class Runner:
                 self.log.info("Status files cleaned up.")
                 start_time = now
 
-            if transcription_id == "None":
+            task_id, task = self.get_oldest_status_file_in_query()
+
+            if task_id == "None":
                 time.sleep(10)
                 continue
 
-            transcription_id = self.get_oldest_status_file_in_query()
-
-            self.log.debug("Processing file: " + transcription_id)
+            self.log.debug("Processing file: " + task_id)
             try:
                 self.data_handler.update_status_file(
-                    TranscriptionStatus.IN_PROGRESS.value, transcription_id
+                    TranscriptionStatus.IN_PROGRESS.value, task_id
                 )
-                # TODO: add check to run translation or transcribe
-                self.transcribe(transcription_id)
+                if task == "transcribe" or task == "align":
+                    self.transcribe(task_id)
+                if task == "translate":
+                    self.translate(task_id)
 
             except Exception as e:
                 self.log.error(f"Runner Exception of type {type(e).__name__}: {str(e)}")
                 self.data_handler.update_status_file(
-                    TranscriptionStatus.ERROR.value, transcription_id, str(e)
+                    TranscriptionStatus.ERROR.value, task_id, str(e)
                 )
                 continue
 
@@ -85,21 +90,53 @@ class Runner:
             return
         self.data_handler.merge_transcript_to_status(transcription_id, response["data"])
 
+    def translate(self, task_id) -> None:
+        """Translates the audio file with the given transcription_id."""
+        transcription = self.data_handler.get_status_file_by_id(task_id)
+
+        # TODO: Move the following in the runner handler
+        transcription["start_time"] = (
+            datetime.now(timezone.utc).replace(microsecond=0).isoformat()
+        )
+        translated_text = translate_text(
+            transcription["transcript"]["text"],
+            transcription["language"],
+            transcription["target_language"],
+        )
+
+        transcription["transcript"] = align_segments(
+            transcription["transcript"], translated_text
+        )
+
+        # This is here to see the difference in segmented tranlation level
+        # for segment in transcription["transcript"]["segments"]:
+        #     segment["text"] = translate_text(
+        #         segment["text"], transcription["language"], target_language
+        #     )
+
+        transcription["language"] = transcription["target_language"]
+        transcription["end_time"] = (
+            datetime.now(timezone.utc).replace(microsecond=0).isoformat()
+        )
+        transcription["status"] = TranscriptionStatus.FINISHED.value
+
+        self.data_handler.write_status_file(task_id, transcription)
+
     def get_oldest_status_file_in_query(
         self,
-        race_condition_sleep_ms: int = 5000,
+        race_condition_sleep: float = 5.0,
         data_handler: DataHandler = DataHandler(),
-    ) -> str:
+    ) -> Tuple[str, str]:
         """Gets the oldest transcription in query."""
         oldest_start_time = None
         oldest_transcription_id: str = None
 
         files = os.listdir(data_handler.status_path)
         if len(files) == 0:
-            return "None"
+            return ("None", "None")
 
         # wait to avoid race conditions between runners
-        time.sleep(random.randint(0, race_condition_sleep_ms) / 1000.0)
+        time.sleep(random.uniform(0, race_condition_sleep))
 
         for filename in os.listdir(data_handler.status_path):
             try:
@@ -110,6 +147,7 @@ class Runner:
                     current_status = data.get("status")
                     start_time = data.get("start_time")
                     model = data.get("model")
+                    task = data.get("task")
 
                     if start_time is None or current_status is None:
                         continue
@@ -138,6 +176,8 @@ class Runner:
                 data_handler.delete_audio_file(transcription_id)
                 continue
 
-        if oldest_transcription_id:
-            return oldest_transcription_id
-        return "None"
+        return (
+            (oldest_transcription_id, task)
+            if oldest_transcription_id
+            else ("None", task)
+        )
