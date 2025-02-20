912a2036640d9f38bdd67f438dcdcfb77e0733b4
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-02-20 20:02:38 +0100

    feat(websockets): rework partial logic

--- src/helper/test/test_local_agreement.py
+++ src/helper/test/test_local_agreement.py
@@ -10,7 +10,7 @@ EXAMPLE_MULTIPLE_SENTENCES = ["This", "is", "a", "test.", "This", "is", "sadly",
 def test_creation():
     testable = LocalAgreement()
     assert testable.get_confirmed_length() == 0
-    assert testable.confirmed_has_sentence_end() is False
+    assert testable.contains_has_sentence_end() is False
 
 def test_merge():
     partial1: List[Word] = [
@@ -33,7 +33,7 @@ def test_merge():
     testable.merge(partial3)
     # Now it should be clear the Error was...well an error?
     assert testable.get_confirmed_text() == "This is a test"
-    assert testable.confirmed_has_sentence_end() is False
+    assert testable.contains_has_sentence_end() is False
 
 def test_merge_with_sentence_end():
     partial1: List[Word] = [
@@ -50,12 +50,12 @@ def test_merge_with_sentence_end():
     testable = LocalAgreement()
     testable.merge(partial1)
     assert len(testable.get_confirmed_text()) == 0
-    assert testable.confirmed_has_sentence_end() is False
+    assert testable.contains_has_sentence_end() is False
     testable.merge(partial2)
 
     testable.merge(partial3)
     assert testable.get_confirmed_text() == "This is a test."
-    assert testable.confirmed_has_sentence_end() is True
+    assert testable.contains_has_sentence_end() is True
 
 def test_clear():
     partial: List[Word] = [
@@ -82,7 +82,7 @@ def test_flush_total():
     ]
 
     assert testable.get_confirmed_length() == len(EXAMPLE_SENTENCE)
-    assert testable.confirmed_has_sentence_end() is False
+    assert testable.contains_has_sentence_end() is False
 
     assert testable.flush_confirmed() == [
         Word(1.0 * i, 1.0 * (i + 1) - 0.1, EXAMPLE_SENTENCE[i], 0.9)
@@ -102,7 +102,7 @@ def test_flush_partial_by_index():
     ]
 
     assert testable.get_confirmed_length() == len(EXAMPLE_SENTENCE)
-    assert testable.confirmed_has_sentence_end() is False
+    assert testable.contains_has_sentence_end() is False
 
     assert testable.flush_confirmed(3) == [
         Word(1.0 * i, 1.0 * (i + 1) - 0.1, EXAMPLE_SENTENCE[i], 0.9) for i in range(3)
@@ -120,7 +120,7 @@ def test_flush_partial_by_sentence_end():
     ]
 
     assert testable.get_confirmed_length() == len(EXAMPLE_MULTIPLE_SENTENCES)
-    assert testable.confirmed_has_sentence_end() is False
+    assert testable.contains_has_sentence_end() is False
 
     assert testable.flush_at_sentence_end() == [
         Word(1.0 * i, 1.0 * (i + 1) - 0.1, EXAMPLE_MULTIPLE_SENTENCES[i], 0.9) for i in range(4)
