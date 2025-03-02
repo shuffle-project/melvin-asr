ce480f85883cd533b648e501d6f835bdb9f48124
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-03-02 17:02:03 +0100

    feat(path handling): fix model handler tests

--- src/rest/test/test_model_handler.py
+++ src/rest/test/test_model_handler.py
@@ -3,22 +3,24 @@ import os
 import shutil
 from src.helper.model_handler import ModelHandler
 
-MODEL_HANDLER_TEST_PATH = "/src/helper/test/test_models/"
+MODEL_HANDLER_TEST_PATH = "./src/helper/test/test_models/"
 
 
 def test_setup_model_and_get_model_path():
     """Setup Should download the model tiny"""
     # see that the model does not exist
-    expected_model_path = os.getcwd() + MODEL_HANDLER_TEST_PATH + "tiny"
+    expected_model_path = os.path.join(os.getcwd(), MODEL_HANDLER_TEST_PATH, "tiny")
     assert not os.path.exists(expected_model_path)
 
     # setup the tiny model
     model_handler = ModelHandler(MODEL_HANDLER_TEST_PATH)
+
+    assert model_handler.get_model_path("tiny") == expected_model_path
+
     res = model_handler.setup_model("tiny")
 
     # see that the tiny model exists
     assert os.path.exists(expected_model_path)
-    assert model_handler.get_model_path("tiny") == expected_model_path
     assert model_handler.get_model("tiny") == expected_model_path
     assert res is True
 
